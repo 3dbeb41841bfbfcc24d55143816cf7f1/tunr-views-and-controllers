e312bf283d93e28059eb47b086eb42bbdb43c32c
king0120 <king0120@gmail.com>
2017-08-25 15:41:25 -0400

    routes-inclass

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,49 +1,49 @@
 class SongsController < ApplicationController
-  # index
-  def index
-    @songs = Song.all
-  end
-
   # new
   def new
-    @song = Song.new
+    @song = artist.songs.new
   end
 
   # create
   def create
-    @song = Song.create(song_params)
-
+    @song = artist.songs.create(song_params)
     redirect_to songs_path(@song)
   end
 
   #show
   def show
-    @song = Song.find(params[:id])
+    @song = artist.songs.find(params[:id])
   end
 
   # edit
   def edit
-    @song = Song.find(params[:id])
+    @song = artist.songs.find(params[:id])
   end
 
   # update
   def update
-    @song = Song.find(params[:id])
+    @song = artist.songs.find(params[:id])
     @song.update(song_params)
 
-    redirect_to songs_path(@song)
+    redirect_to artist_song_path(artist, @song)
   end
 
   # destroy
   def destroy
-    @song = Song.find(params[:id])
+    @song = artist.songs.find(params[:id])
     @song.destroy
 
-    redirect_to songs_path
+    redirect_to artist_path(artist)
+  end
+
+
+  def artist
+    Artist.find(params[:artist_id])
   end
 
   private
   def song_params
     params.require(:song).permit(:title, :album, :preview_url, :artist_id)
   end
+
 end
